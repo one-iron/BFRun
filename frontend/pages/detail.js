503aa22099b6767b153d427c2ed7f73f07c1e2b6
oneiron <admin@adminui-MacBookPro.local>
2020-06-28 09:58:47 +0900

    페이지 라우팅 설정

---
+++ frontend/pages/detail.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Detail = () => {
+  return (
+    <>
+      <TestDiv>Detail Page</TestDiv>
+    </>
+  );
+};
+
+export default Detail;
+
+const TestDiv = styled.div`
+  margin-top: 100px;
+`;
