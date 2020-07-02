c21636d4fcd6f9e388af85f60d27d2a43562131b
yeeunlee <chloe.yeeunl@gmail.com>
2020-07-02 18:40:26 +0900

    db connection 세팅 및 예시 user dao 생성

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,23 @@
-from flask import Flask
+from flask      import Flask
 from flask_cors import CORS
+from user.model import UserDao
 
 
 def create_app(test_config=None):
+
     app = Flask(__name__)
     app.debug = True
     app.config["JSON_SORT_KEYS"] = False
     app.config.from_pyfile("config.py")
     CORS(app, resources={r"*": {"origins": "*"}})
+
+    user_dao = UserDao()
+    video_dao = VideoDao()
+
+    user_service = UserService(user_dao)
+    video_service = VideoService(video_dao)
+
+    create_user_endpoints(app, user_service)
+    create_video_endpoints(app, video_service)
 
     return app
