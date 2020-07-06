e4201992f15a4e75137086f75113c7e2a32822e3
Sung Jun Jin <Sung Jun Jin>
2020-07-06 15:18:01 +0900

    카테고리 API 수정, 비디오 리스트 API 크리에이터별 필터링 구현

--- backend/video/controller.py
+++ backend/video/controller.py
@@ -1,14 +1,24 @@
+from flask import request
 
 
 def create_video_endpoints(app, video_service):
-
     @app.route("/categories", methods=["GET"])
     def get_categories():
         category_response = video_service.get_category_lists()
         return category_response, 200
 
-    @app.route('/video/<int:video_id>', methods=['GET'])
+    @app.route("/video/<int:video_id>", methods=["GET"])
     def get_video_detail(video_id):
         video_detail, video_playlist = video_service.get_video_detail(video_id)
-        return {'video_detail'   : video_detail,
-                'video_playlist' : video_playlist}, 200
+        return {"video_detail": video_detail, "video_playlist": video_playlist}, 200
+
+    @app.route("/videos", methods=["GET"])
+    def get_video_lists():
+        try:
+            params = request.args
+            video_list_response = video_service.get_video_lists(params)
+            return video_list_response, 200
+
+        except Exception as e:
+            return {"message": e}, 400
+
--- backend/video/service.py
+++ backend/video/service.py
@@ -1,5 +1,6 @@
 import yaml
 
+
 class VideoService:
     def __init__(self, video_dao):
         self.video_dao = video_dao
@@ -7,6 +8,7 @@ class VideoService:
     def get_category_lists(self):
         # 카테고리
         categories = {}
+        stacks = []
         # 콘텐츠 타입
         contents_types = self.video_dao.get_contents_types()
         with open("const.yaml") as yaml_const:
@@ -16,17 +18,67 @@ class VideoService:
             frontend_stacks = self.video_dao.get_stacks(const["frontend"])
             backend_stacks = self.video_dao.get_stacks(const["backend"])
 
+            stacks.append({"general_stacks": general_stacks})
+            stacks.append({"frontend_stacks": frontend_stacks})
+            stacks.append({"backend_stacks": backend_stacks})
+
         # 크리에이터 채널
         channels = self.video_dao.get_channels()
 
         categories["content_types"] = contents_types
-        categories["general_stacks"] = general_stacks
-        categories["frontend_stacks"] = frontend_stacks
-        categories["backend_stacks"] = backend_stacks
+        categories["stacks"] = stacks
         categories["channels"] = channels
 
         return categories
 
     def get_video_detail(self, video_id):
-        video_detail, video_playlist = self.video_dao.get_video_detail(video_id), self.video_dao.get_video_playlist(video_id)
+        video_detail, video_playlist = (
+            self.video_dao.get_video_detail(video_id),
+            self.video_dao.get_video_playlist(video_id),
+        )
         return video_detail, video_playlist
+
+    def get_video_lists(self, params):
+
+        filters = {}
+        video_lists = {}
+        # 필터 parameter binding
+        stacks = []
+        channels = []
+        contents_types = params.get("contents_types_id", None)
+        stacks = params.getlist("stack_id", None)
+        channels = params.getlist("channels_id", None)
+
+        # 콘텐츠 타입 필터
+        if contents_types:
+            video_lists["videos"] = self.video_dao.get_contents_types_videos(
+                contents_types
+            )
+            return video_lists
+
+        # 스택 필터
+        if contents_types is None and len(channels) == 0:
+            videos = []
+            for stack in stacks:
+                # videos.append(self.video_dao.get_stack_videos(stack))
+                video_lists[stack] = self.video_dao.get_stack_videos(stack)
+
+            return video_lists
+
+        # 채널, 스택 필터
+        for channel in channels:
+            videos = []
+            for stack in stacks:
+                filters = {}
+                filters["stack_id"] = stack
+                filters["channel_id"] = channel
+                stack_videos = self.video_dao.get_videos(filters)
+
+                if stack_videos == 0:
+                    stack_videos = ""
+                videos += stack_videos
+
+            channel_name = self.video_dao.get_channel_name(channel)
+            video_lists[channel_name[0]["name"]] = videos
+
+        return video_lists
