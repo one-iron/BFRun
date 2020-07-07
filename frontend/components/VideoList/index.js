902166cd64c910f972b666bd780ec64aeb405ff5
mj <hi.minjungkim@gmail.com>
2020-07-07 21:27:58 +0900

    fetch data

--- frontend/components/VideoList/index.js
+++ frontend/components/VideoList/index.js
@@ -1,34 +1,25 @@
 // external modules
 import styled from 'styled-components';
-import axios from 'axios';
-import React, { useState, useEffect } from 'react';
-import { RECOMMEND } from '../../config';
+import React, { useState } from 'react';
 
 // internal modules
 import BFTest from '../BFTest';
 import Recommend from './Recommend';
-import Test from './Test';
 
 const VideoList = (props) => {
-  const [recommended, setRecommended] = useState();
-
-  // useEffect(() => {
-  //   axios
-  //     .get('https://run.mocky.io/v3/257ee40e-be84-4f39-8a53-aba9e5691ddf')
-  //     .then((res) => setRecommended(res.data));
-  // }, []);
-
-  // console.log('props video list', props.recommendList);
   return (
     <VideoWrap>
       <BFTest />
       <article>
-        {/* <Recommend title="전체" recommended={props.recommendList.general} /> */}
-        <Test title="전체Test" recommended={props.recommendList.general} />
-        <Test title="프론트Test" recommended={props.recommendList.front} />
-        <Test title="백Test" recommended={props.recommendList.back} />
-        {/* <Recommend title="프론트엔드 추천 영상" />
-        <Recommend title="백엔드 추천 영상" /> */}
+        <Recommend
+          title="전체 추천영상"
+          recommended={props.recommendList.general}
+        />
+        <Recommend
+          title="프론트 추천영상"
+          recommended={props.recommendList.front}
+        />
+        <Recommend title="백 추천영상" recommended={props.recommendList.back} />
       </article>
     </VideoWrap>
   );
