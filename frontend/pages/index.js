25e01383e29c14527f6e4f0924d752f58419a206
mj <hi.minjungkim@gmail.com>
2020-07-07 20:30:16 +0900

    filter url list

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,13 +1,14 @@
 // external modules
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 
 // internal modules
 import Nav from '../components/Nav';
 import Category from '../components/Category';
 import VideoList from '../components/VideoList';
 import SelectedVideo from '../components/SelectedVideo';
-import { CATEGORY, RECOMMEND } from '../config';
+import { CATEGORY, SELECTED_VIDEO_LIST, RECOMMEND } from '../config';
 
 export async function getStaticProps() {
   const res = await fetch(CATEGORY);
@@ -26,50 +27,101 @@ export default function HomePage(props) {
   const stackList = categoryfromAPI.stacks;
   const creatorList = categoryfromAPI.channels;
   const [selectedContent, setSelectedContent] = useState([]);
+  const [contentId, setContentId] = useState([]);
   const [selectedStack, setSelectedStack] = useState([]);
+  const [stackId, setStackId] = useState([]);
   const [selectedCreator, setSelectedCreator] = useState([]);
+  const [creatorId, setCreatorId] = useState([]);
+
+  // 보여줄 태그 url axios
+  useEffect(() => {
+    let returnUrl = '';
+    const splitContentId = contentId.join('&');
+    const splitStackId = stackId.join('&');
+    const splitCreatorId = creatorId.join('&');
+
+    if (splitContentId) {
+      returnUrl = splitContentId;
+    } else if (!splitContentId) {
+      if (splitStackId && splitCreatorId) {
+        returnUrl = `${splitStackId}&${splitCreatorId}`;
+      } else if (splitStackId) {
+        returnUrl = splitStackId;
+      } else if (splitCreatorId) {
+        returnUrl = splitCreatorId;
+      }
+    }
+    console.log('return', returnUrl);
+    axios
+      .get(`${SELECTED_VIDEO_LIST}?${returnUrl}`)
+      .then((res) => console.log(res));
+  });
 
   // 컨텐츠 태그 추가/제거
-  const addDelContentTags = (tag) => {
+  const addDelContentTags = (name, id) => {
     if (selectedStack[0] || selectedCreator[0]) {
+      // 스택이나 크리에이터 태그가 있다면 지우고, 컨텐츠 태그로 대체
       setSelectedStack([]);
       setSelectedCreator([]);
-      setSelectedContent([tag]);
-    } else if (selectedContent.includes(tag)) {
-      setSelectedContent(selectedContent.filter((tags) => tags !== tag));
+      setSelectedContent([name]);
+      setStackId([]);
+      setContentId([`contents_types_id=${id}`]);
+    } else if (selectedContent.includes(name)) {
+      setSelectedContent(selectedContent.filter((names) => names !== name));
+      setContentId(
+        contentId.filter((ids) => ids !== `contents_types_id=${id}`),
+      );
+      setContentId([]);
     } else {
-      setSelectedContent([tag]);
+      setSelectedContent([name]);
+      setContentId([`contents_types_id=${id}`]);
     }
   };
 
   // 스택 태그 추가/제거
-  const addDelStackTags = (tag) => {
+  const addDelStackTags = (name, id) => {
     if (selectedContent[0]) {
+      // content가 선택되었다면 content는 없애고, 스택 태그 추가
       setSelectedContent([]);
-      setSelectedStack([tag]);
-    } else if (selectedStack.includes(tag)) {
-      setSelectedStack(selectedStack.filter((tags) => tags !== tag));
+      setSelectedStack([name]);
+      setContentId([]);
+      setStackId([`stack_id=${id}`]);
+    } else if (selectedStack.includes(name)) {
+      setSelectedStack(selectedStack.filter((names) => names !== name));
+      setStackId(stackId.filter((ids) => ids !== `stack_id=${id}`));
     } else if (selectedStack.length > 2) {
       // 최대 3개만 가능
-      setSelectedStack(selectedStack.slice(1, 3).concat(tag));
+      setSelectedStack(selectedStack.slice(1, 3).concat(name));
+      setStackId(stackId.slice(1, 3).concat(`stack_id=${id}`));
     } else {
-      setSelectedStack([...selectedStack, tag]);
+      setSelectedStack([...selectedStack, name]);
+      setStackId([...stackId, `stack_id=${id}`]);
     }
   };
 
   // 크리에이터 태그 추가/제거
-  const addDelCreatorTags = (tag) => {
+  const addDelCreatorTags = (tag, id) => {
     if (selectedContent[0]) {
       setSelectedContent([]);
       setSelectedCreator([tag]);
+      setContentId([]);
+      setCreatorId([`channels_id=${id}`]);
     } else if (selectedStack.length > 0) {
       // stack이 있을 경우 하나의 크리에이더만 선택 가능
       setSelectedCreator([tag]);
+      setCreatorId([`channels_id=${id}`]);
+      // stack이 있으면서 선택한 creator를 없애고 싶은 경우
+      if (selectedCreator.includes(tag)) {
+        setSelectedCreator(selectedCreator.filter((tags) => tags !== tag));
+        setCreatorId(creatorId.filter((ids) => ids !== `channels_id=${id}`));
+      }
     } else {
       // stack이 없을 경우 모든 크리에이터 선택 가능
       setSelectedCreator([...selectedCreator, tag]);
+      setCreatorId([...creatorId, `channels_id=${id}`]);
       if (selectedCreator.includes(tag)) {
         setSelectedCreator(selectedCreator.filter((tags) => tags !== tag));
+        setCreatorId(creatorId.filter((ids) => ids !== `channels_id=${id}`));
       }
     }
   };
